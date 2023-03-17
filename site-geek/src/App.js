ef0ce707b404a4e2a5af858a8c997cf936d95f09
Wendel Paes <107985828+WendelPaesx@users.noreply.github.com>
2023-03-17 02:29:14 -0300

    .

---
+++ site-geek/src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <header className ="header">
+        <div>
+            <h2>Dezembro Promocional</h2>
+            <p>Produtos selecionados com 33% de desconto</p>
+            <button type="submit"> Ver Consoles</button>
+        </div>
+
+
+    </header>
+      
+    </div>
+  );
+}
+
+export default App;
