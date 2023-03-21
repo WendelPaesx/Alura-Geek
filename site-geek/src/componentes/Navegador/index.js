50810b993dcf1d08768c12be224483de9837bc69
Wendel Paes <107985828+WendelPaesx@users.noreply.github.com>
2023-03-21 02:54:18 -0300

    .

--- site-geek/src/componentes/Navegador/index.js
+++ site-geek/src/componentes/Navegador/index.js
@@ -1,12 +1,13 @@
 
 import './navegador.css'
+import logo from '../img/Logo.png'
 
 
 function Navegador() {
     return (
         <><div className='nav'>
             <div className='menu'>
-                <img src="" alt='Logo'></img>
+                <img src={logo} alt='Logo'></img>
                 <input type="search" name="buscador" id="buscador" placeholder=" O que deseja encontrar?" />
             </div>
             <button id="login" type="submit">Login</button>
